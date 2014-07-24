4afa05d70ca5de375f036d6510afcf9f3ae9158e
vinthony <vinthony@gmail.com>
2014-07-24 23:17:56 +0800

    fix bugs

--- lzw.js
+++ lzw.js
@@ -20,7 +20,7 @@ var lzwEncode = function(input,initDic){
 	while(inputArr.length>0){	
 		var curr = inputArr.shift();//取得下一个
 		var it = prev +""+ curr;//连接
-		if(it in dic){//如果在字典中,将整体继续看作是字典进入下轮循环
+		if(dic[it]){//如果在字典中,将整体继续看作是字典进入下轮循环
 			prev = it;
 		}else{//没有在字典中,将前驱置入字典中,增长字典,重新开始计数 
 			code.push(dic[prev]); 
@@ -32,17 +32,42 @@ var lzwEncode = function(input,initDic){
 		code.push(dic[prev]);
 	var re = {};
 	re.code = code;
-	re.dic = dic;
-	console.log(re);
+	re.dic = initDic;
+	console.log(code);
+	console.log(dic);
 	return re;
 }
 var lzwDecode = function(input){
 	var code = input.code;//编码的code
-	var dic = input.dic;//编码的字典
-	var re = "";
-	for (var i = 0; i < code.length; i++) {
-		re += getKeyByValue(dic,code[i]);
-	};
+	var dic = deDic(input.dic);//编码的字典
+	var result = "";
+	var temp = "";
+	console.log(code);
+	var prev = code.shift(); //result T
+	result = dic[prev];
+	while(code.length>0){
+		curr = code.shift();//当前编码
+		if(getKeyByValue(dic,dic[curr])){//在字典中
+			off = curr;
+		}else{//不在字典中
+			// console.log('不在字典中',dic[prev],dic[curr]);
+			// dic[len(dic)] = dic[prev] + temp.charAt(0);
+			// off = dic[curr];
+		}
+		result += dic[off];
+		dic[len(dic)] = dic[prev] + dic[off].charAt(0);
+		prev = off;	
+
+	}
+	  console.log(dic);
+	  console.log(result);
+}
+
+function deDic(dic){
+	var re = {};
+	for (var i = 0; i < dic.length; i++) {
+			re[i] = dic[i] ;
+		};
 	return re;
 }
 function len(obj){
@@ -60,7 +85,7 @@ function getKeyByValue(o,v){
 	}
 	return "";
 }
-console.log();
-var encode = lzwEncode("TOBEORNOTTOBEORTOBEORNOT");
+var encode = lzwEncode("TOBEORNOTTOBEORTOBEORNOT",['B','E','N','O','R','T']);
 var decode = lzwDecode(encode);
-console.log(JSON.stringify(encode).length/"TOBEORNOTTOBEORTOBEORNOT".length);
+console.log("TOBEORNOTTOBEORTOBEORNOT");
+
